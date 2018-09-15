f0c9e346b3bde9afc6d6c7a9031de1e4c849ffba
Lazloo <lars.freier@posteo.de>
2018-09-15 22:29:55 +0200

    20180915 - Modify Starting script
    Signed-off-by: Lazloo <lars.freier@posteo.de>

--- patch_image.py
+++ patch_image.py
@@ -1,10 +1,13 @@
 from sklearn.feature_extraction import image
+import operator
 import time as time
+from tqdm import tqdm
 import GPR_Package.GPR_Script as GP
 import matplotlib.pyplot as plt
 from PIL import Image
 import numpy as np
 import os as os
+import pickle
 
 
 def read_label(filename, label_choices):
@@ -25,38 +28,54 @@ def read_labels(dir_name, ext='.png', label_choices='0'):
     n_examples = label_array.shape[0]
     n_labels = label_array.shape[1]
     n_label_choices = label_array.shape[2]
-    label_array = label_array.reshape([n_examples, n_label_choices*n_labels])
+    label_array = label_array.reshape([n_examples, n_label_choices * n_labels])
     # for fn in os.listdir(dir_name):
     #     if fn.endswith(ext):
     #         fd = os.path.join(dir_name, fn)
     #         labels.append(read_label(fd, label_choices))
     return label_array
 
+
 # Todo: no threshold as otherwise a picture is purely black + normalizing
 def read_images(dir_name, ext='.png'):
     fd = [os.path.join(dir_name, fn) for fn in os.listdir(dir_name) if fn.endswith(ext)]
     im_raw = [Image.open(iFile).convert('L') for iFile in fd]
     image_data = [np.asarray(i) for i in im_raw]
-    image_data = [i/255 for i in image_data]
+    image_data = [i / 255 for i in image_data]
     # binary_image_array = [(iData > 125.5) * 1.0 for iData in image_data]
     # image_data/255
     return image_data
 
 
 def generate_patch_aggregation(images_array, scaling_factor=[2, 2]):
-    patches = [image.extract_patches_2d(image=iImage,
-                                        patch_size=(round(iImage.shape[0] / scaling_factor[0]),
-                                                    round(iImage.shape[1] / scaling_factor[1])))
-               for iImage in images_array]
-    features = np.array([[np.sum(iPatch) for iPatch in iImage] for iImage in patches])
+    features_list = \
+        [
+            [
+                np.sum(iPatch)
+                for iPatch
+                in image.extract_patches_2d(image=iImage,
+                                            patch_size=(round(iImage.shape[0] / scaling_factor[0]),
+                                                        round(iImage.shape[1] / scaling_factor[1])))
+            ]
+            for iImage in images_array
+        ]
 
-    return features
+    return np.array(features_list)
 
 
-# label_array = read_labels('images/test_data_images/for_testing', label_choices='0123456789')
-label_array = read_labels('images/test_data_images/for_testing', label_choices='01')
-images_array = read_images('images/test_data_images/for_testing')
+# dir = 'images/test_data_images/for_testing'
+dir = 'images/real_2/test'
+dir_test = 'images/real_2/test_short'
+label_choices = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+# label_choices='0123456789'
+
+label_array = read_labels(dir, label_choices=label_choices)
+images_array = read_images(dir)
 features = generate_patch_aggregation(images_array, scaling_factor=[2, 2])
+
+label_array_test = read_labels(dir_test, label_choices=label_choices)
+images_array_test = read_images(dir_test)
+features_test = generate_patch_aggregation(images_array_test, scaling_factor=[2, 2])
 
 gp_test = GP.GPR_Class()
 
@@ -64,8 +83,19 @@ t1 = time.time()
 gp_test.create_GPR_model(features, label_array)
 t2 = time.time()
 print('GPR Modelling took: ' + str(t1 - t2) + 's')
-gp_test.predict(features[0])
 
+m, s = gp_test.predict(features_test[2])
+indices = np.where(m > 1e-10)[1]
+print(label_choices[m[0][0:62].argmax()])
+print(label_choices[m[0][63:].argmax()])
+
+file_pi = open('filename_2Letters.obj', 'wb')
+pickle.dump(obj=gp_test, file=file_pi)
+
+test_array = [False] * len(label_array)
+for iTest in tqdm(range(0, len(label_array))):
+    m, s = gp_test.predict(features[iTest])
+    test_array[iTest] = ((label_array[iTest] == 1) == (m > 0.1)).sum() == len(label_array)
 
 # gp_test.plot_1D_interpolation()
 #
