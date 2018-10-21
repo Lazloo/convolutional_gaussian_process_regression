40af31783653fdc6eed4ebaf8aaad086de5d8eed
Lazloo <lars.freier@posteo.de>
2018-10-21 18:42:20 +0200

    20181021 - Introduce GPU
    Signed-off-by: Lazloo <lars.freier@posteo.de>

--- GPR_Package/GPR_Script.py
+++ GPR_Package/GPR_Script.py
@@ -1,5 +1,9 @@
 from sklearn.feature_extraction import image
 from matplotlib import pyplot as plt
+import logging
+import gpflow
+import time as time
+import sys
 import numpy as np
 from operator import itemgetter
 from sklearn.gaussian_process import GaussianProcessRegressor
@@ -14,11 +18,41 @@ import os
 class GPR_Class:
 
     def __init__(self):
+
+        self.logger = logging.getLogger('GPR Classfication')
+        self.gp_package = 'sklearn'
+
+        self.verbose = True
+        self.verbose_debug = True
+
+        self.gp = GaussianProcessRegressor()
+
         self.data = []
         self.x_min = []
         self.x_max = []
         self.y_min = []
         self.y_max = []
+        self.X = []
+        self.Y = []
+
+        self.label_array = np.array([])
+        self.label_array_train = np.array([])
+        self.label_array_test = np.array([])
+        self.label_array_eval = np.array([])
+
+        self.images_array = np.array([])
+        self.images_array_train = np.array([])
+        self.images_array_test = np.array([])
+        self.images_array_eval = np.array([])
+
+        self.indices_distributed = np.array([])
+        self.train_indices = np.array([])
+        self.test_indices = np.array([])
+        self.evaluation_indices = np.array([])
+
+        self.features_train = np.array([])
+        self.features_test = np.array([])
+        self.features_eval = np.array([])
 
     @staticmethod
     def normalize(x: List[float], min_in: float, max_in: float, min_out: float, max_out: float) -> list:
@@ -31,30 +65,85 @@ class GPR_Class:
         self.y_min = np.amin(Y, axis=0)
         self.y_max = np.amax(Y, axis=0)
         assert np.min(self.y_min) < np.max(self.y_max), 'y_min should be smaller than y_max'
-        assert np.min(self.x_min) < np.max(self.x_max), 'x_min should be smaller than x_max'
 
+        for i in range(len(self.x_min)):
+            if self.x_min[i] == self.x_max[i]:
+                if abs(self.x_max[i]) > 0:
+                    add_value = min(1e-1, abs(self.x_max[i] * 1e-2))
+                else:
+                    add_value = 1e-1
+
+                self.x_max[i] += add_value
+                # self.logger.warning('Added ' + str(add_value) + ' to maximum of variable ' +
+                #                     str(i) + ' as min and max was equal')
+
+        assert np.min(self.x_min) < np.max(self.x_max), 'x_min should be smaller than x_max'
         self.X = X
         self.Y = Y
 
+        t1 = time.time()
         x_norm = self.normalize(X, min_in=self.x_min, max_in=self.x_max, min_out=0, max_out=1)
+        t2 = time.time()
+        if self.verbose_debug:
+            print('normalizing X took : ' + str(t2 - t1) + 's')
+
+        t1 = time.time()
         y_norm = self.normalize(Y, min_in=self.y_min, max_in=self.y_max, min_out=0, max_out=1)
+        t2 = time.time()
+        if self.verbose_debug:
+            print('normalizing Y took : ' + str(t2 - t1) + 's')
+
         kernel = 1.0 * Matern(length_scale=0.1, length_scale_bounds=(1e-5, 1e5), nu=2.5) + WhiteKernel()
-        self.gp = GaussianProcessRegressor(kernel=kernel, alpha=0.0).fit(x_norm, y_norm)
+        t1 = time.time()
+        if self.gp_package == 'sklearn':
+            self.gp = GaussianProcessRegressor(kernel=kernel, alpha=0.0).fit(x_norm, y_norm)
+        elif self.gp_package == 'gp_flow':
+            self.gp = GaussianProcessRegressor(kernel=kernel, alpha=0.0).fit(x_norm, y_norm)
+            self.gp = gpflow.models.GPR(x_norm, y_norm,
+                                        kern=gpflow.kernels.Matern52(input_dim=x_norm.shape[1]) +
+                                             gpflow.kernels.Linear(input_dim=x_norm.shape[1]) +
+                                             gpflow.kernels.White(input_dim=x_norm.shape[1])
+                                        )
+            self.gp.compile()
+            opt = gpflow.train.ScipyOptimizer()
+            opt.minimize(self.gp)
+        else:
+            raise AssertionError('unknown GP Method: ' + self.gp_package)
+        t2 = time.time()
+        if self.verbose_debug:
+            print('training GPR : ' + str(t2 - t1) + 's')
 
         return self.gp
 
     def predict(self, x_test):
-        x_interpolation = self.normalize(x_test,
-                                         min_in=self.x_min, max_in=self.x_max,
-                                         min_out=0, max_out=1)
-        y_mean_interpol, y_cov_norm = self.gp.predict(x_interpolation[np.newaxis, :], return_cov=True)
 
-        y_mean = self.normalize(y_mean_interpol,
-                                min_in=0, max_in=1,
-                                min_out=self.y_min, max_out=self.y_max)
-        y_cov = y_cov_norm * ((self.y_max - self.y_min) ** 2)
+        x_interpolation = x_test
+        if len(x_test.shape) == 1:
+            x_interpolation = x_interpolation[np.newaxis, :]
 
-        return y_mean, y_cov
+        n_test_points = x_interpolation.shape[0]
+        for i in range(n_test_points):
+            x_interpolation[i] = self.normalize(x_interpolation[i],
+                                                min_in=self.x_min, max_in=self.x_max,
+                                                min_out=0, max_out=1)
+
+        if self.gp_package == 'sklearn':
+            y_mean_interpol, y_std_norm = self.gp.predict(x_interpolation, return_std=True)
+        elif self.gp_package == 'gp_flow':
+            y_mean_interpol, y_var_norm = self.gp.predict_y(x_interpolation)
+            y_std_norm = y_var_norm ** 0.5
+        else:
+            raise AssertionError('unknown GP Method: ' + self.gp_package)
+
+        y_mean = y_mean_interpol
+        y_std = y_std_norm
+        for i in range(n_test_points):
+            y_mean[i] = self.normalize(y_mean[i],
+                                       min_in=0, max_in=1,
+                                       min_out=self.y_min, max_out=self.y_max)
+            y_std[i] = y_std_norm[i] * ((max(self.y_max) - min(self.y_min)) ** 2)
+
+        return y_mean, y_std
 
     def plot_1D_interpolation(self):
         x_test = np.linspace(0, 1, 1000)
@@ -132,19 +221,28 @@ class GPR_Class:
         return np.array(features_list)
 
     @staticmethod
-    def convolution_step_edges_max_pooling(im_array, windows_size: tuple = (10, 10)):
-        im_array_convolve = scipy.signal.convolve(in1=im_array, in2=[[1, -1], [-1, 1]], mode="valid")
+    def convolution_step_edges_max_pooling(im_array,
+                                           windows_size: tuple,
+                                           weights: np.array = np.array([]),
+                                           weight_shape: list = []):
+        if len(weights) > 0:
+            if len(weight_shape) == 0:
+                raise AssertionError('weight_shape is empty')
+            weight_matrix = weights.reshape(weight_shape)
+        else:
+            weight_matrix = [[1, -1], [-1, 1]]
+        im_array_convolve = scipy.signal.convolve(in1=im_array, in2=weight_matrix, mode="valid")
         im_array_convolve_max = im_array_convolve * (
-                im_array_convolve == scipy.ndimage.filters.maximum_filter(input=im_array_convolve, size=(3, 3))
+                im_array_convolve == scipy.ndimage.filters.maximum_filter(input=im_array_convolve, size=windows_size)
         )
         im_array_convolve_max_reduce = measure.block_reduce(im_array_convolve_max, windows_size, np.max)
 
         return im_array_convolve_max_reduce
 
     @staticmethod
-    def generate_indices_train_test_evaluation(label_array, ratio: float = 0.8):
+    def generate_indices_train_test_evaluation(label_array, ratio: tuple = (0.8, 0.1, 0.1)):
 
-        assert sum(ratio) <= 1, 'ratio has to be smaller equal to 1.0'
+        assert (max(ratio) <= 1) & (min(ratio) >= 0), 'Each entry of ratio must be between 0.0 and 1.0'
 
         unique_label = len(label_array[0])
         indices = [None] * unique_label
@@ -168,8 +266,8 @@ class GPR_Class:
             n_test = round((n_data_partial - n_train) / 2)
             n_evaluation = n_data_partial - n_train - n_test
             # 0:X does not include X
-            n_test_actual = round(n_test*ratio[1])
-            n_evaluation_actual = round(n_evaluation*ratio[2])
+            n_test_actual = round(n_test * ratio[1])
+            n_evaluation_actual = round(n_evaluation * ratio[2])
             indices_distributed[i] = [
                 itemgetter(*indices_rand[0:n_train])(indices[i]),
                 itemgetter(*indices_rand[n_train:n_train + n_test_actual])(indices[i]),
@@ -183,3 +281,100 @@ class GPR_Class:
             evaluation_indices += indices_distributed[i][2]
 
         return indices_distributed, train_indices, test_indices, evaluation_indices
+
+    def generate_train_test_eval_indices(self, ratio_train_test_eval: tuple):
+        self.indices_distributed, self.train_indices, self.test_indices, self.evaluation_indices = \
+            self.generate_indices_train_test_evaluation(self.label_array, ratio=ratio_train_test_eval)
+
+    def load_data(self, dir_name: str, label_choices: str, ratio_train_test_eval: tuple):
+        self.label_array = self.read_labels(dir_name=dir_name, label_choices=label_choices, output_type='binary')
+        self.images_array = self.read_images(dir_name=dir_name)
+        self.generate_train_test_eval_indices(ratio_train_test_eval)
+
+        self.images_array_train = itemgetter(*self.train_indices)(self.images_array)
+        self.images_array_test = itemgetter(*self.test_indices)(self.images_array)
+        self.images_array_eval = itemgetter(*self.evaluation_indices)(self.images_array)
+
+        self.label_array_train = itemgetter(*self.train_indices)(self.label_array)
+        self.label_array_test = itemgetter(*self.test_indices)(self.label_array)
+        self.label_array_eval = itemgetter(*self.evaluation_indices)(self.label_array)
+
+    def generate_test_train_eval_data(self,
+                                      windows_size: tuple,
+                                      weights: np.array = np.array([]),
+                                      weight_shape: list = []):
+        self.features_train = [self.convolution_step_edges_max_pooling(i, windows_size=windows_size,
+                                                                       weights=weights,
+                                                                       weight_shape=weight_shape).flatten()
+                               for i in self.images_array_train]
+        self.features_test = [self.convolution_step_edges_max_pooling(i, windows_size=windows_size,
+                                                                      weights=weights,
+                                                                      weight_shape=weight_shape).flatten()
+                              for i in self.images_array_test]
+        self.features_eval = [self.convolution_step_edges_max_pooling(i, windows_size=windows_size,
+                                                                      weights=weights,
+                                                                      weight_shape=weight_shape).flatten()
+                              for i in self.images_array_eval]
+
+    def train_and_test(self,
+                       windows_size: tuple,
+                       weights=np.array([]),
+                       weight_shape: list = []):
+
+        # reset to integer
+        windows_size = tuple([int(round(i)) for i in windows_size])
+
+        t1 = time.time()
+        self.generate_test_train_eval_data(windows_size=windows_size, weights=weights, weight_shape=weight_shape)
+        t2 = time.time()
+        if self.verbose_debug:
+            print('generating data took : ' + str(t2 - t1) + 's')
+        print('windows_size: ' + str(windows_size) + '- weights: ', weights)
+
+        t1 = time.time()
+        self.create_GPR_model(
+            self.features_train,
+            self.label_array_train
+        )
+        t2 = time.time()
+        if self.verbose_debug:
+            print('generating GPR model took : ' + str(t2 - t1) + 's')
+
+        t1 = time.time()
+        pred_vec = self.predict(np.array(self.features_test))
+        predicted_output = [pred_vec[0][i].argmax()
+                            for i in range(len(self.features_test))]
+        t2 = time.time()
+        if self.verbose_debug:
+            print('test prediction took : ' + str(t2 - t1) + 's')
+
+        t1 = time.time()
+        test_accuracy = len([predicted_output[i] for i in range(len(predicted_output)) if
+                             predicted_output[i] == self.label_array_test[i].argmax()
+                             ]) / len(predicted_output)
+        t2 = time.time()
+        if self.verbose_debug:
+            print('test accuracy took : ' + str(t2 - t1) + 's')
+        # test_accuracy = len([predicted_output[i] for i in range(len(predicted_output)) if
+        #                      predicted_output[i] == (itemgetter(*self.test_indices)(self.label_array))[i].argmax()
+        #                      ])/len(predicted_output)
+        if self.verbose:
+            pred_vec = self.predict(np.array(self.features_train))
+            predicted_output = [pred_vec[0][i].argmax()
+                                for i in range(len(self.features_train))]
+            train_accuracy = len([predicted_output[i] for i in range(len(predicted_output)) if
+                                  predicted_output[i] == self.label_array_train[i].argmax()
+                                  ]) / len(predicted_output)
+
+            pred_vec = self.predict(np.array(self.features_eval))
+            predicted_output = [pred_vec[0][i].argmax()
+                                for i in range(len(self.features_eval))]
+            eval_accuracy = len([predicted_output[i] for i in range(len(predicted_output)) if
+                                 predicted_output[i] == self.label_array_eval[i].argmax()
+                                 ]) / len(predicted_output)
+
+            print('Train accuracy: ' + str(train_accuracy) +
+                  ' - Test accuracy: ' + str(test_accuracy) +
+                  ' - Evaluation Accuracy: ' + str(eval_accuracy))
+
+        return test_accuracy
--- test_convoulution.py
+++ test_convoulution.py
@@ -1,9 +1,24 @@
 from scipy import signal as sg
 import scipy as scipy
-from skimage.transform import rescale, resize, downscale_local_mean
+from  skimage.transform import rescale, resize, downscale_local_mean
+import skimage.measure as measure
 import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image
+
+
+def convolution_step_edges_max_pooling(im_array):
+    im_array_convolve = scipy.signal.convolve(in1=im_array, in2=[[1, -1], [-1, 1]], mode="valid")
+    bool_max = im_array_convolve*(
+                   im_array_convolve == scipy.ndimage.filters.maximum_filter(input=im_array_convolve, size=(3, 3))
+           ) != 0
+    im_array_convolve_max = im_array_convolve*(
+            im_array_convolve == scipy.ndimage.filters.maximum_filter(input=im_array_convolve, size=(3, 3))
+    )
+    im_array_convolve_max_reduce = measure.block_reduce(im_array_convolve_max, (2, 2), np.max)
+
+    return im_array_convolve_max_reduce
+
 
 dir_name = 'C:/Users/Lazloo/test/all/short/7_02124.png'
 im = Image.open(dir_name).convert('L')
@@ -13,17 +28,27 @@ im_array = np.asarray(im)
 
 # plt.imshow(im_array)
 # plt.show()
-im_array_convolve = scipy.signal.convolve(in1=im_array, in2=[[1, -1], [-1, 1]], mode="full")
+im_array_convolve = scipy.signal.convolve(in1=im_array, in2=[[1, -1], [-1, 1]], mode="valid")
+
+
+bool_max = im_array_convolve*\
+           (
+                   im_array_convolve == scipy.ndimage.filters.maximum_filter(input=im_array_convolve, size=(3, 3))
+           ) != 0
+im_array_convolve_max = im_array_convolve*(
+        im_array_convolve == scipy.ndimage.filters.maximum_filter(input=im_array_convolve, size=(3, 3))
+)
+im_array_convolve_max_rescale = rescale(image=im_array_convolve_max, scale=0.5, mode='reflect', preserve_range = True)
+im_array_convolve_max_reduce = measure.block_reduce(im_array_convolve_max, (2, 2), np.max)
+im_array_convolve_max_reduce_2 = measure.block_reduce(im_array_convolve, (2, 2), np.max)
+
+plt.show()
+plt.imshow(im_array)
 plt.imshow(im_array_convolve)
-plt.show()
+plt.imshow(im_array_convolve_max)
+plt.imshow(im_array_convolve_max_rescale)
+plt.imshow(im_array_convolve_max_reduce)
+plt.imshow(im_array_convolve_max_reduce_2)
 
-bool_max = im_array_convolve*(im_array_convolve == scipy.ndimage.filters.maximum_filter(input=im_array_convolve, size=(3, 3))) != 0
-im_array_convolve[bool_max]
-a = im_array_convolve*(im_array_convolve == scipy.ndimage.filters.maximum_filter(input=im_array_convolve, size=(3, 3)))
-plt.imshow(a)
-plt.show()
-b = rescale(image=a, scale=0.5, mode='reflect', preserve_range = True)
-plt.imshow(b)
-plt.show()
+plt.imshow(convolution_step_edges_max_pooling(im_array=im_array))
 
-scipy.misc.imshow(im_array_convolve)
